4615db28682ac01d00e828687976d935cf8fdbf4
Daniel Tuohy <danielr2e@gmail.com>
2025-08-13 13:06:51 -0400

    gen model script

---
+++ gen-model.js
@@ -0,0 +1,4 @@
+require('ts-node/register');
+const { generateFromEcore } = require('./src/lib/source-generators/modelgenutils');
+
+generateFromEcore(process.argv[2] || './src/__tests__/TmfTest.ecore');
