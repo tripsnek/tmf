0e0b81578b8651507742f512981fd7dda90e5641
Daniel Tuohy <danielr2e@gmail.com>
2025-08-22 00:19:55 -0400

    epackage registry

---
+++ src/lib/metamodel/e-package-registry.ts
@@ -0,0 +1,25 @@
+import { EPackage } from './epackage';
+
+export class EPackageRegistry {
+  pathToPkg = new Map<string, EPackage>();
+
+  /** Singleton */
+  private static eINSTANCE: EPackageRegistry;
+
+  // Add a getter that ensures proper initialization order
+  public static getInstance(): EPackageRegistry {
+    if (!this.eINSTANCE) {
+      // Ensure CorePackage is initialized first
+      this.eINSTANCE = new EPackageRegistry();
+    }
+    return this.eINSTANCE;
+  }
+
+  public register(path: string, pkg: EPackage){
+    this.pathToPkg.set(path,pkg);
+  }
+
+  public getPackage(path: string){
+    return this.pathToPkg.get(path);
+  }
+}
